302fd4db3b55cbfe85b1b07a13b59f8e919f90e6
arlindi3 <draboarlind@gmail.com>
2025-05-25 17:08:10 +0200

    Ndryshime në konfigurim për deploy me GitHub Pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         {/* <Route path="/" element={<Login />} /> */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/todo" element={<Todo />} />
         <Route path="/bmicalc" element={<BmiCalc />} />
         <Route path="/game" element={<Game />} />
